94565fdd9ab21d7d8bd6475e764b18d875fdb3c3
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2022-03-29 09:27:59 +0200

    Update 1.3
    Add fixedstep camera control

--- src/hook.cpp
+++ src/hook.cpp
@@ -4,69 +4,104 @@ void HookJMP(DWORD dwAddress, DWORD dwFunction)
 {
 	DWORD dwOldProtect, dwNewProtect, dwNewCall;
 	dwNewCall = dwFunction - dwAddress - 5;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<BYTE*>(dwAddress) = 0xE9;
-	*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<BYTE*>(dwAddress) = 0xE9;
+		*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
+		VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(dwAddress), 5);
+	}
 }
 
 void HookCall(DWORD dwAddress, DWORD dwFunction)
 {
 	DWORD dwOldProtect, dwNewProtect, dwNewCall;
 	dwNewCall = dwFunction - dwAddress - 5;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<BYTE*>(dwAddress) = 0xE8;
-	*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<BYTE*>(dwAddress) = 0xE8;
+		*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
+		VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 5, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(dwAddress), 5);
+	}
 }
 
 void HookCallN(DWORD dwAddress, DWORD dwFunction)
 {
 	DWORD dwOldProtect, dwNewProtect, dwNewCall;
 	dwNewCall = dwFunction - dwAddress - 5;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 6, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<BYTE*>(dwAddress) = 0xE8;
-	*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
-	*reinterpret_cast<BYTE*>(dwAddress + 5) = 0x90;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 6, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 6, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<BYTE*>(dwAddress) = 0xE8;
+		*reinterpret_cast<DWORD*>(dwAddress + 1) = dwNewCall;
+		*reinterpret_cast<BYTE*>(dwAddress + 5) = 0x90;
+		VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), 6, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(dwAddress), 6);
+	}
 }
 
-void WriteStack(DWORD dwAddress, BYTE* stack, DWORD len)
+void WriteStack(DWORD dwAddress, const char* stack, DWORD len)
 {
 	DWORD dwOldProtect, dwNewProtect;
-	VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), len, PAGE_READWRITE, &dwOldProtect);
-	memcpy(reinterpret_cast<LPVOID>(dwAddress), stack, len);
-	VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), len, dwOldProtect, &dwNewProtect);
+	if (VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), len, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		memcpy(reinterpret_cast<LPVOID>(dwAddress), stack, len);
+		VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), len, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(dwAddress), len);
+	}
 }
 
 void Nop(DWORD dwAddress, int size)
 {
 	DWORD dwOldProtect, dwNewProtect;
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), size, PAGE_READWRITE, &dwOldProtect);
-    memset(reinterpret_cast<LPVOID>(dwAddress), 0x90, size);
-    VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), size, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), size, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		memset(reinterpret_cast<LPVOID>(dwAddress), 0x90, size);
+		VirtualProtect(reinterpret_cast<LPVOID>(dwAddress), size, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(dwAddress), size);
+	}
 }
 
 void OverWriteByte(DWORD addressToOverWrite, BYTE newValue)
 {
     DWORD dwOldProtect, dwNewProtect;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 1, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<BYTE*>(addressToOverWrite) = newValue;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 1, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 1, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<BYTE*>(addressToOverWrite) = newValue;
+		VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 1, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(addressToOverWrite), 1);
+	}
 }
 
 void OverWriteWord(DWORD addressToOverWrite, WORD newValue)
 {
     DWORD dwOldProtect, dwNewProtect;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 2, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<WORD*>(addressToOverWrite) = newValue;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 2, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 2, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<WORD*>(addressToOverWrite) = newValue;
+		VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 2, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(addressToOverWrite), 2);
+	}
 }
 
 void OverWrite(DWORD addressToOverWrite, DWORD newValue)
 {
     DWORD dwOldProtect, dwNewProtect;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, PAGE_READWRITE, &dwOldProtect);
-	*reinterpret_cast<DWORD*>(addressToOverWrite) = newValue;
-    VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, dwOldProtect, &dwNewProtect);
+	if(VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<DWORD*>(addressToOverWrite) = newValue;
+		VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(addressToOverWrite), 4);
+	}
+}
+
+void OverWriteFloat(DWORD addressToOverWrite, float newValue)
+{
+    DWORD dwOldProtect, dwNewProtect;
+	if(VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+	{
+		*reinterpret_cast<float*>(addressToOverWrite) = newValue;
+		VirtualProtect(reinterpret_cast<LPVOID>(addressToOverWrite), 4, dwOldProtect, &dwNewProtect);
+		FlushInstructionCache(GetCurrentProcess(), reinterpret_cast<LPVOID>(addressToOverWrite), 4);
+	}
 }
--- src/hook.h
+++ src/hook.h
@@ -4,8 +4,15 @@
 void HookJMP(DWORD dwAddress, DWORD dwFunction);
 void HookCall(DWORD dwAddress, DWORD dwFunction);
 void HookCallN(DWORD dwAddress, DWORD dwFunction);
-void WriteStack(DWORD dwAddress, BYTE* stack, DWORD len);
+void WriteStack(DWORD dwAddress, const char* stack, DWORD len);
 void Nop(DWORD dwAddress, int size);
 void OverWriteByte(DWORD addressToOverWrite, BYTE newValue);
 void OverWriteWord(DWORD addressToOverWrite, WORD newValue);
 void OverWrite(DWORD addressToOverWrite, DWORD newValue);
+void OverWriteFloat(DWORD addressToOverWrite, float newValue);
+
+template<typename T, size_t len>
+void WriteStack(DWORD dwAddress, T(&stack)[len])
+{
+	WriteStack(dwAddress, reinterpret_cast<const char*>(stack), len - 1);
+}
